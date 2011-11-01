ce0babcd6056d25509092a4016a178c057297e64
Paul O’Shannessy <paul@oshannessy.com>
2011-11-01 11:38:42 -0700

    add sorting for ss-sizeof-tabdata

--- ss-sizeof-tabdata.js
+++ ss-sizeof-tabdata.js
@@ -1,3 +1,14 @@
+function compare(a,b) {
+  /* sort by order */
+  /* return 0; */
+  /* sort by size (low - high) */
+  /* return a.size - b.size; */
+  /* sort by size (high - low) */
+  return b.size - a.size;
+  /* sort by url */
+  /* return a.url.localeCompare(b.url); */
+}
+
 var {classes: Cc, interfaces: Ci, utils: Cu} = Components;
 Cu.import("resource://gre/modules/DownloadUtils.jsm");
 var ss = Cc["@mozilla.org/browser/sessionstore;1"].getService(Ci.nsISessionStore);
@@ -6,10 +17,16 @@ var state = JSON.parse(ss.getBrowserState());
 var lines = [];
 state.windows.forEach(function(win, windex) {
   lines.push("window " + windex);
+  var tabs = [];
   win.tabs.forEach(function(tab) {
     var url = tab.entries[tab.index - 1].url;
     var len = JSON.stringify(tab).length;
-    lines.push("\t" + url + " - " + DownloadUtils.convertByteUnits(len).join(""));
+    tabs.push({ url: url, size: len});
   });
+  tabs.sort(compare);
+  tabs = tabs.map(function(t) {
+    return "\t" + t.url + " - " + DownloadUtils.convertByteUnits(t.size).join("");
+  });
+  lines = lines.concat(tabs);
 });
 lines.join("\n");
